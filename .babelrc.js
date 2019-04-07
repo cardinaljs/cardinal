17278ff254353aa17b12c9784d360979f4869a48
CalebPitan <36812893+CalebPitan@users.noreply.github.com>
2019-04-07 16:33:28 +0100

    babel configuration for transpiling

---
+++ .babelrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        loose: true,
+        modules: false,
+        exclude: ["transform-typeof-symbol"]
+      }
+    ]
+  ],
+  plugins: ["@babel/plugin-proposal-class-properties"]
+  }
