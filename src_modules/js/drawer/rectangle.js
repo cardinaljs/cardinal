c876dbc15fe5524f8d1f5de4fbee9552910286aa
CalebPitan <36812893+CalebPitan@users.noreply.github.com>
2019-04-11 13:38:24 +0100

    `Rectangle` class now extends the `Vector` class
    Renamed get methods `wGTh` & `hGTw` to have better meaning

--- src_modules/js/drawer/rectangle.js
+++ src_modules/js/drawer/rectangle.js
@@ -1,37 +1,25 @@
-export default class Rectangle {
-  /**
-   *
-   * @param {number} x1
-   * @param {number} y1
-   * @param {number} x2
-   * @param {number} y2
-   */
+import {Vector} from './vector'
+
+export class Rectangle extends Vector {
   constructor(x1, y1, x2, y2) {
-    this.coordsX = {
-      x1,
-      x2
-    }
-    this.coordsY = {
-      y1,
-      y2
-    }
+    super(x1, y1, x2, y2)
   }
 
   // getter
   get width() {
-    return Math.abs(this.coordsX.x2 - this.coordsX.x1)
+    return Math.abs(this.displacementX)
   }
 
   get height() {
-    return Math.abs(this.coordsY.y2 - this.coordsY.y1)
+    return Math.abs(this.displacementY)
   }
 
   // methods
-  wGTh() {
-    return this.width() > this.height()
+  get greaterWidth() {
+    return this.width > this.height
   }
 
-  hGTw() {
-    return !this.wGTh()
+  get greaterHeight() {
+    return !this.greaterWidth
   }
 }
