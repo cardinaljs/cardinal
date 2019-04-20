e08071a38d49371857aa53f078b4ef79351d48d6
CalebPitan <36812893+CalebPitan@users.noreply.github.com>
2019-04-20 15:37:18 +0100

    Enumeration of directions
    Add enumeration of directions so it can be accessible from on here and not
    `SnappedDrawer` class

--- src/drawer/index.js
+++ src/drawer/index.js
@@ -2,4 +2,9 @@ import SnappedDrawer from './snapped-drawer'
 
 export default class Drawer {
   static SnappedDrawer = SnappedDrawer
+  // enum
+  static UP = SnappedDrawer.UP
+  static LEFT = SnappedDrawer.LEFT
+  static DOWN = SnappedDrawer.DOWN
+  static RIGHT = SnappedDrawer.RIGHT
 }
