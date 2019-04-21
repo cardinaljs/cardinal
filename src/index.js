a5bd27ae879d00c57799776e6ec4ddc9e4414b10
CalebPitan <36812893+CalebPitan@users.noreply.github.com>
2019-04-21 22:32:43 +0100

    add 'circularpath' module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import _CircularPath from './circular-path'
 import _Drawer from './drawer'
 import _Nav from './nav'
 
 export default class Cardinal {
+  static CircularPath = _CircularPath
   static Drawer = _Drawer
   static Nav = _Nav
 }
