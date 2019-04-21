f76c1d699577bf3879bce31a4c086708048d5251
CalebPitan <36812893+CalebPitan@users.noreply.github.com>
2019-04-21 22:33:40 +0100

    circle module used in circular-path

---
+++ src/circular-path/circle.js
@@ -0,0 +1,32 @@
+export const POINT_ANGLE = 360
+export const HALF = 1 / 2
+export const PI = Math.PI
+export const RAD = PI / (POINT_ANGLE * HALF)
+
+export class Circle {
+  constructor(radius) {
+    /**
+     * @type {number}
+     */
+    this.radius = radius
+    this.diameter = this.radius * 2
+  }
+
+  get area() {
+    return PI * this.radius ** 2
+  }
+
+  get circumference() {
+    return 2 * PI * this.radius
+  }
+
+  areaOfSect(angle) {
+    angle = angle * RAD
+    return angle / POINT_ANGLE * this.area
+  }
+
+  lenOfSect(angle) {
+    angle = angle * RAD
+    return angle / POINT_ANGLE * this.circumference
+  }
+}
