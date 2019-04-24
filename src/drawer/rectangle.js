ae0eefed94f186013b184437614b277ee5d7a7be
CalebPitan <36812893+CalebPitan@users.noreply.github.com>
2019-04-24 14:54:13 +0100

    Reverted
    Tried to make the Rectangle which is Scalar convert to Vector and
    block access to Vector properties and methods on default, though it
    extends Vector. The Vector props. and methods will be available when
    a `toVector` is called. This resulted in getting
    "bugs as much as characters in this code". Hence the change was reverted

--- src/drawer/rectangle.js
+++ src/drawer/rectangle.js
@@ -3,22 +3,15 @@ import { Vector } from './vector'
 export class Rectangle extends Vector {
   constructor(x1, y1, x2, y2) {
     super(x1, y1, x2, y2)
-    this._isVector = false
   }
 
   // getter
   get width() {
-    if(!this._isVector) {
-      return Math.abs(this.displacementX)
-    }
-    return undefined
+    return Math.abs(this.displacementX)
   }
 
   get height() {
-    if (!this._isVector) {
-      return Math.abs(this.displacementY)
-    }
-    return undefined
+    return Math.abs(this.displacementY)
   }
 
   get greaterWidth() {
@@ -27,32 +20,5 @@ export class Rectangle extends Vector {
 
   get greaterHeight() {
     return !this.greaterWidth
-  }
-
-  get displacementX() {
-    if (this._isVector) {
-      return super.displacementX
-    }
-    return undefined
-  }
-
-  get displacementY() {
-    if (this._isVector) {
-      return super.displacementY
-    }
-    return undefined
-  }
-
-  toVector() {
-    this._isVector = true
-    return this
-  }
-
-  toScalar() {
-    if (!this._isVector) {
-      return undefined
-    }
-    this._isVector = false
-    return this
   }
 }
