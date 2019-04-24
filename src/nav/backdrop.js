64d74c7cbea8fab3c4101d426431a02c7d9f8e7d
CalebPitan <36812893+CalebPitan@users.noreply.github.com>
2019-04-24 14:37:44 +0100

    fix error in backdrop transition
    the `s` -- seconds notation for transition timing was omitted. Now added
    and fixes sudden/swift removal of backdrop

--- src/nav/backdrop.js
+++ src/nav/backdrop.js
@@ -9,14 +9,14 @@ export class Backdrop {
     css(this.backdrop, {
       display: 'block',
       opacity: 1,
-      transition: `opacity linear ${time / 1e3}`
+      transition: `opacity linear ${time / 1e3}s`
     })
   }
 
   hide(time) {
     css(this.backdrop, {
       opacity: 0,
-      transition: `opacity linear ${time / 1e3}`
+      transition: `opacity linear ${time / 1e3}s`
     })
     window.setTimeout(() => {
       css(this.backdrop, {
