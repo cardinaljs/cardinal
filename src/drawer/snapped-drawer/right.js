3287494d5924965a00cfee494862420719d51dbe
CalebPitan <36812893+CalebPitan@users.noreply.github.com>
2019-05-04 02:32:08 +0100

    A functional "Right" snapped-drawer
    create a working right-drawer

--- src/drawer/snapped-drawer/right.js
+++ src/drawer/snapped-drawer/right.js
@@ -1,4 +1,10 @@
-import Final from './../../util'
+import {
+  ZERO,
+  validateThreshold
+} from './../../util'
+import {
+  Rectangle
+} from './../rectangle'
 
 const DIRECTION = 'right'
 const DIMENSION = 'dimension'
@@ -6,6 +12,12 @@ const DISPLACEMENT = 'displacement'
 const EVENT_OBJ = 'event'
 const THRESHOLD = 'threshold'
 const BELOW_THRESHOLD = 'belowthreshold'
+const OPEN = 'open'
+const CLOSE = 'close'
+const UNIT = 'px'
+const MAX_START_AREA = 25
+const THRESHOLD_VALUE = 0.667
+const FALSE_TOUCH_START_POINT = 2
 
 export default class Right {
   /**
@@ -15,34 +27,41 @@ export default class Right {
    */
   constructor(options) {
     this.options = options
-    // the target element, not listening to,
-    // but reacting to event
+    /**
+     * Drawer Element
+     * @type {HTMLElement}
+     */
     this.element = options.ELEMENT
     /**
-     * @type number
-     * Size of device window in pixels
+     * Size of device window
+     *
+     * unused: required in `Right` and `Bottom`
+     * @type {Function}
      */
-    this.winSize = this.options.sizeOfWindow || window.screen.availWidth
+    this._winSize = this.options.sizeOfWindow || Right._windowSize
     /**
-     * @type number
-     * A numerical representation of the
-     * target element's width
+     * @type {number}
      */
-    this.width = this.element.offsetWidth
-    this.unit = this.options.UNIT
+    this.winSize = this._winSize()
     /**
-     * @type number
-     * A maximum area where the draw-start is sensitive
+     * @type {number}
      */
-    this.maxArea = this.winSize - this.options.maxStartArea
+    this.width = this.options.SIZE
+    this.unit = this.options.unit || UNIT
+    /**
+     * @type {number}
+     * A minimum area where the draw-start is sensitive
+     */
+    this.minArea = this.winSize - (this.options.maxStartArea || MAX_START_AREA)
 
     /**
      * A threshold which the `touchmove` signal must attain
      * before being qualified to stay shown
-     * the threshold should be a value between `1.0` and `0`
-     * @type number
+     * the threshold should be a value between `0` and `1.0`
+     * @type {number}
      */
-    this.threshold = this.options.threshold
+    this.threshold = this.options.threshold || THRESHOLD_VALUE
+    this.threshold = validateThreshold(this.threshold)
 
     // Touch coordinates (Touch Start)
     this.startX = -1
@@ -50,6 +69,9 @@ export default class Right {
     // Touch coordinates (Touch Move)
     this.resumeX = -1
     this.resumeY = -1
+    // Touch coordinates (Touch End) [these may not be important]
+    this.endX = -1
+    this.endY = -1
 
     /**
      * A control for scroll. This control prevents
@@ -57,53 +79,85 @@ export default class Right {
      * the (&delta;`X`) coords and (&delta;`Y`) coords.
      * Utilising the `Rectangle` class to get bounds
      * and isolate territories
-     * @type boolean
+     * @type {boolean}
      */
     this.scrollControlSet = false
     this.scrollControl = null
+    this.timing = {
+      /**
+       * @type {Date}
+       */
+      start: null,
+      /**
+       * @type {Date}
+       */
+      end: null
+    }
 
-    this.context = null
+    this._context = this
   }
 
-  start() {
+  /**
+   * The `touchstart` event handler for the `Left` drawer `class`
+   * @param {TouchEvent} e an event `object`: An event `object`
+   * representing an `object` of all `properties` related
+   * to the `touchstart` event.
+   * @param {Function} fn - a callback function called when the `start`
+   * event is triggered
+   * @returns {void}
+   */
+  start(e, fn) {
+    this._loopWinSizeChangeEvent()
+    const WIN_WIDTH = this.winSize
+    this.timing.start = new Date()
     const start = e.changedTouches[0].pageX || e.changedTouches[0].clientX
     this.startX = start
     this.startY =  e.changedTouches[0].pageY || e.changedTouches[0].clientY
     /**
-     * The `Drawer`'s `Right` class uses the `CSS property` `Right`
+     * The `Drawer`'s `Left` class uses the `CSS property`, `left`
      * for updating and defining position of the drawn element
      */
     const currentPosition = parseFloat(
-      this.element.style.right.replace(
+      Right._getStyle(this.element)[DIRECTION].replace(
         /[^\d]*$/, ''
       )
     )
-    const dimension = `-${this.width - (this.winSize - start)}${this.unit}`
-    const displacement = `-${this.width - Final.START}${this.unit}`
+    this.positionOnStart = currentPosition
+    const dimension = `-${this.width - (WIN_WIDTH - start)}${this.unit}`
+    const displacement = `-${this.width - (WIN_WIDTH - FALSE_TOUCH_START_POINT)}${this.unit}`
 
-    if (start >= Final.ZERO && start <= this.maxArea && currentPosition !== Final.ZERO) {
-      this.element.style.right = displacement || dimension
-      // Block every scroll job on the screen
-      document.body.style.overflow = Final.HIDDEN
+    if (start <= WIN_WIDTH && start >= this.minArea && currentPosition !== ZERO) {
+      const response = {
+        [EVENT_OBJ]: e,
+        [DIMENSION]: dimension,
+        [DISPLACEMENT]: displacement
+      }
+      fn.call(this._context, response, new Rectangle(this.startX, this.startY, -1, -1))
     }
   }
 
   /**
    * The `touchmove` event handler for the `Right` drawer `class`
-   * @param {*} e an event `object`: An event `object`
+   * @param {TouchEvent} e an event `object`: An event `object`
    * representing an `object` of all `properties` related
-   * to the `touchstart` event.
+   * to the `touchmove` event.
+   * @param {Function} fn - a callback function called when the `move`
+   * event is triggered
+   * @returns {void}
    */
-  move() {
+  move(e, fn) {
+    const WIN_WIDTH = this.winSize
+    const FALSE_WIDTH = WIN_WIDTH - this.width
     const resume = e.changedTouches[0].pageX || e.changedTouches[0].clientX
     this.resumeX = resume
     this.resumeY = e.changedTouches[0].pageY || e.changedTouches[0].clientY
 
     const currentPosition = parseFloat(
-      this.element.style[DIRECTION].replace(
+      Right._getStyle(this.element)[DIRECTION].replace(
         /[^\d]*$/, ''
       )
     )
+    const nextAction = this.positionOnStart === ZERO ? CLOSE : OPEN
 
     const start = this.startX
     const width = this.width
@@ -112,7 +166,8 @@ export default class Right {
      * of the element ignore `start` and use `width`
      * as starting point.
      */
-    const virtualStart = start < (this.winSize - width) ? this.winSize - width : start
+    const virtualStart = start < FALSE_WIDTH ? FALSE_WIDTH : start
+
     /**
      * Dimension for opening. When the drawer is being opened,
      * the `width` is the max dimension, and the `start` can
@@ -128,7 +183,7 @@ export default class Right {
      * as the `right` property of the `HTMLElement.style` is the one being updated and not the left,
      * so the css `right` property is an enough respect for its direction.
      *
-     * *__If it should be respected then__*,
+     * *__If it should be respected then:__*,
      * 1. The `Right Drawer class` would be updating `left css property` and not `right`, i.e, initialy an element that uses the `Right Drawer` must have a `css file` that defines a `css left property` for the element and not a `right` property; As in
      * ```scss
      * .menu {
@@ -141,7 +196,8 @@ export default class Right {
      * This way there is no `right` property respecting the direction
      * So the vector attribute of the dimension is preserved.
      */
-    const dimension = `-${width - (this.winSize - resume)}${this.unit}`
+    const dimension = `-${width - (WIN_WIDTH - resume)}${this.unit}`
+
     /**
      * Dimension for closing. When the drawer is being closed,
      * the `width` is the max dimension and the `start` could
@@ -153,29 +209,134 @@ export default class Right {
      * `width`, the width becomes the start point else the `start`
      */
     const vdimension = `-${resume - virtualStart}${this.unit}`
-    new Final()
-    const backdropSheet = {
-      opacity: (this.winSize - resume) / width,
-      display: Final.DISPLAY
-    }
     const rect = new Rectangle(
       this.startX,
       this.startY,
       this.resumeX,
       this.resumeY
     )
-    const isBoundX = rect.wGTh()
-    // eslint-disable-next-line no-unused-expression, no-sequence, no-extra-parens
-    !this.scrollControlSet &&
-    (this.scrollControl = isBoundX), (this.scrollControlset = !this.scrollControlSet)
+    const isBoundX = rect.greaterWidth
+
+    if (!this.scrollControlSet) {
+      this.scrollControl = isBoundX
+      this.scrollControlSet = !this.scrollControlSet
+    }
+
+    // OPEN LOGIC
+    if (start <= WIN_WIDTH && start >= this.minArea && currentPosition !== ZERO && isBoundX && nextAction === OPEN && this.scrollControl && rect.displacementX < ZERO) {
+      const response = {
+        [EVENT_OBJ]: e,
+        [DIMENSION]: dimension,
+        open: true,
+        close: false
+      }
+      fn.call(this._context, response, rect)
+    }
+
+    // CLOSE LOGIC
+    if (resume >= FALSE_WIDTH && currentPosition <= this.width && isBoundX && nextAction === CLOSE && this.scrollControl && rect.displacementX > ZERO) {
+      const response = {
+        [EVENT_OBJ]: e,
+        [DIMENSION]: vdimension,
+        close: true,
+        open: false
+      }
+      fn.call(this._context, response, rect)
+    }
   }
 
-  end() {
+  /**
+   * The `touchend` event handler for the `Right` drawer `class`
+   * @param {TouchEvent} e an event `object`: An event `object`
+   * representing an `object` of all `properties` related
+   * to the `touchend` event.
+   * @param {Function} fn - a callback function called when the `end`
+   * event is triggered
+   * @param {{}} thresholdState - a state object which should be passed
+   * by reference for updating by this method
+   * @returns {void}
+   */
+  end(e, fn, thresholdState) {
+    this.timing.end = new Date()
+    const WIN_WIDTH = this.winSize
+    const FALSE_WIDTH = WIN_WIDTH - this.width
+    const end = e.changedTouches[0].pageX || e.changedTouches[0].clientX
+    this.endX = end
+    this.endY = e.changedTouches[0].pageY || e.changedTouches[0].clientY
 
+    const rect = new Rectangle(this.startX, this.startY, this.endX, this.endY)
+
+    const start = this.startX
+    const TIMING = this.timing.end.getTime() - this.timing.start.getTime()
+    const threshold = this.threshold
+    const signedOffsetSide = parseFloat(
+      Right._getStyle(this.element)[DIRECTION].replace(
+        /[^\d]*$/, ''
+      )
+    )
+    const nonZero = `-${this.width}px`
+    const zero = `${ZERO}`
+    const offsetSide = Math.abs(signedOffsetSide)
+    let action = OPEN
+    // release the control for another session
+    this.scrollControl = this.scrollControlSet = false // eslint-disable-line no-multi-assign
+
+    const nextAction = this.positionOnStart === ZERO ? CLOSE : OPEN
+    const response = {
+      [EVENT_OBJ]: e,
+      position: signedOffsetSide,
+      rect
+    }
+
+    function getResponse(state, trueForOpen) {
+      const opposite = 'oppositeDimension'
+      if (state === THRESHOLD && trueForOpen || state === BELOW_THRESHOLD && !trueForOpen) {
+        return {
+          [DIMENSION]: zero,
+          TIMING,
+          [opposite]: nonZero,
+          ...response
+        }
+      } else if (state === THRESHOLD && !trueForOpen || state === BELOW_THRESHOLD && trueForOpen) {
+        return {
+          [DIMENSION]: nonZero,
+          TIMING,
+          [opposite]: zero,
+          ...response
+        }
+      }
+      return {}
+    }
+
+    // OPEN LOGIC
+    if (nextAction === OPEN && start >= this.minArea) {
+      if (offsetSide <= this.width * threshold) {
+        thresholdState.state = [THRESHOLD, CLOSE]
+        thresholdState.stateObj = getResponse(thresholdState.state[0], true)
+      } else {
+        thresholdState.state = [BELOW_THRESHOLD, CLOSE]
+        thresholdState.stateObj = getResponse(thresholdState.state[0], true)
+      }
+      fn.call(this, action)
+      return
+    }
+
+    // CLOSE LOGIC
+    if (nextAction === CLOSE && rect.displacementX > ZERO && FALSE_WIDTH - this.resumeX <= this.width) {
+      action = CLOSE
+      if (offsetSide >= this.width * threshold) {
+        thresholdState.state = [THRESHOLD, OPEN]
+        thresholdState.stateObj = getResponse(thresholdState.state[0], false)
+      } else {
+        thresholdState.state = [BELOW_THRESHOLD, OPEN]
+        thresholdState.stateObj = getResponse(thresholdState.state[0], false)
+      }
+      fn.call(this, action)
+    }
   }
 
   setContext(ctx) {
-    this.context = ctx
+    this._context = ctx
     return this
   }
 
@@ -183,10 +344,19 @@ export default class Right {
     return pseudoElt ? window.getComputedStyle(elt, pseudoElt) : window.getComputedStyle(elt)
   }
 
+  static _windowSize() {
+    return window.screen.availWidth
+  }
+
   // no need for `window.onorientationchange`
-  _loopWinSizeChangeEvent() {
-    window.setInterval(() => {
-      this.winSize = window.screen.availWidth
-    }, 1e3)
+  _loopWinSizeChangeEvent(getHandler) {
+    const fn = () => {
+      this.winSize = typeof this.winSize === 'function' ? this._winSize() : Right._windowSize()
+      this.minArea = this.winSize - (this.options.maxStartArea || MAX_START_AREA)
+    }
+    if (getHandler) {
+      return fn
+    }
+    window.setInterval(fn, 1e3)
   }
 }
