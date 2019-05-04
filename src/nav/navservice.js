52af119f5e0ea85ca3ddf2dbfb705838575d4789
CalebPitan <36812893+CalebPitan@users.noreply.github.com>
2019-05-04 02:34:23 +0100

    fix lint errors

--- src/nav/navservice.js
+++ src/nav/navservice.js
@@ -1,12 +1,12 @@
 import {
-  css,
-  NAV_BOX_SHADOW
+  NAV_BOX_SHADOW,
+  css
 } from './../util'
 
-const TRANSITION_STYLE = "ease"
-const EFFECT = "transition"
-const TRANS_END = "transitionend"
-const UNIT = "px"
+const TRANSITION_STYLE = 'ease'
+const EFFECT = 'transition'
+const TRANS_END = 'transitionend'
+const UNIT = 'px'
 
 class NavService {
   constructor(options) {
@@ -18,37 +18,40 @@ class NavService {
     this.event = options.DEFAULT_EVENT || 'click'
     this.direction = options.DIRECTION
     this.width = this.nav.offsetWidth
-    this.trans_time = options.TRANSITION/1e3
-    this.transition = `${this.direction} ${TRANSITION_STYLE} ${this.trans_time}s`
+    this.transTime = options.TRANSITION / 1e3
+    this.transition = `${this.direction} ${TRANSITION_STYLE} ${this.transTime}s`
     // state of the nav, whether open or close
     this.alive = false
+    // diff. btw. event triggered from Drawer class and on here
+    this._closeClicked = false
   }
 
   _width(unit) {
-    unit = unit || ""
+    unit = unit || ''
     return this.width + unit
   }
 
   activate() {
     this.button.addEventListener(this.event, (e) => {
-      this.handler.call(this, e)
+      this.handler(e)
     })
     this.backdropElement.addEventListener(this.event, () => {
       this._close()
     })
     this.nav.addEventListener(TRANS_END, () => {
-      if (!this.alive) {
+      if (!this.alive && this._closeClicked) {
         this._cleanShadow()
+        this._closeClicked = false
       }
     })
     return 0
   }
 
   handler() {
-    let state = NavService.css(this.nav, this.direction).
-      replace(/[^\d]*$/, '')
-    state = /\.(?=\d)/.test(state) ? Math.floor(parseFloat(state)) : parseInt(state)
-    if (`${state}${UNIT}` == `-${this._width(UNIT)}`) {
+    let state = NavService.css(this.nav, this.direction)
+      .replace(/[^\d]*$/, '')
+    state = /\.(?=\d)/.test(state) ? Math.floor(parseFloat(state)) : parseInt(state, 10)
+    if (`${state}${UNIT}` === `-${this._width(UNIT)}`) {
       this._open()
     } else {
       this._close()
@@ -56,7 +59,7 @@ class NavService {
   }
 
   _open() {
-    let style = {
+    const style = {
       [this.direction]: 0,
       [EFFECT]: this.transition,
       boxShadow: NAV_BOX_SHADOW
@@ -67,7 +70,7 @@ class NavService {
   }
 
   _close() {
-    let style = {
+    const style = {
       [this.direction]: `-${this._width(UNIT)}`,
       [EFFECT]: this.transition
       // don't clean shadow here
@@ -76,6 +79,7 @@ class NavService {
     NavService.css(this.nav, style)
     this.backdrop.hide(this.options.TRANSITION)
     this.alive = false
+    this._closeClicked = true
   }
 
   _cleanShadow() {
@@ -87,8 +91,7 @@ class NavService {
   }
 
   deactivate() {
-    this.button.removeEventListener(this.event, this.handler)
-    return 0
+    throw ReferenceError('cannot deactivate API specified as default. This service must be kept running')
   }
 }
 
