5cd57d644fcb9795822278d6c4ed34f19c8d30aa
CalebPitan <36812893+CalebPitan@users.noreply.github.com>
2019-05-18 18:45:46 +0100

    a more functional nav-drawer

--- src/nav/drawer.js
+++ src/nav/drawer.js
@@ -1,24 +1,34 @@
 import {
+  Bound,
   DIRECTIONS,
-  NAV_BOX_SHADOW
+  NAV_BOX_SHADOW,
+  css,
+  getAttribute,
+  getData
 } from './../util'
 import Drawer from './../drawer/'
+import STATE from './state'
 
 const ZERO = 0
 const KILO = 1e3
-const MIN_TIME_TO_OVERRIDE_BELOWTHRESHOLD = 0.7
-const MIN_POSITIVE_DISPLACEMENT = 40
+const MIN_TIME_TO_OVERRIDE_BELOWTHRESHOLD = 0.5
+const MIN_POSITIVE_DISPLACEMENT = 10
 const MIN_NEGATIVE_DISPLACEMENT = -MIN_POSITIVE_DISPLACEMENT
 const TRANSITION_STYLE = 'linear'
 const EFFECT = 'transition'
-const TRANS_TIMING = '0.1s' // This value is basic, the calc'ed value will depend on drawer speed
+const OVERFLOW = 'overflow'
+const TRANS_TIMING = '0.1s'
 const TRANS_TEMPLATE = `${TRANSITION_STYLE} ${TRANS_TIMING}`
 const HIDDEN = 'hidden'
 const SCROLL = 'scroll'
+const HREF = 'href'
+const HASH_ATTR = `data-${HREF}`
 const START = 'start'
 const MOVE = 'move'
 const THRESHOLD = 'threshold'
 const BELOW_THRESHOLD = `below${THRESHOLD}`
+const MAX_TIME = KILO
+const MAX_SPEED = 500
 
 class NavDrawer {
   /**
@@ -30,37 +40,23 @@ class NavDrawer {
    */
   constructor(options) {
     this.options = options
-    /**
-     * @type {HTMLElement}
-     */
     this.element = this.options.ELEMENT
-    /**
-     * @type {HTMLBodyElement}
-     */
     this._body = this.options.BODY
     this._backdrop = this.options.BACKDROP
-    /**
-     * @type {number}
-     */
     this.direction = this.options.DIRECTION
 
-    this.checkDirection()
+    this._checkDirection()
 
     this.directionString = DIRECTIONS[this.direction]
+    this.bound = this._bound
 
     const o = {
       ...options,
       SIZE: this.elementSize,
       TARGET: document
     }
-    this.drawer = new Drawer.SnappedDrawer(o)
+    this.drawer = new Drawer.SnappedDrawer(o, this.bound)
     this.transition = `${this.directionString} ${TRANS_TEMPLATE}`
-  }
-
-  checkDirection() {
-    if (this.direction !== Drawer.LEFT && this.direction !== Drawer.RIGHT) {
-      throw RangeError('Direction out of range')
-    }
   }
 
   activate() {
@@ -86,17 +82,33 @@ class NavDrawer {
     return this.element.offsetWidth
   }
 
+  /**
+   * @returns {Bound} a boundary object: Bound
+   */
+  get _bound() {
+    const curPos = css(this.element, this.directionString)
+      .replace(/[^\d]*$/, '')
+    const upperBound = this.elementSize
+    const lowerBound = upperBound + parseInt(curPos, 10)
+    return new Bound(lowerBound, upperBound)
+  }
+
   _startHandler(response) {
-    this.element.style[this.directionString] = response.displacement
-    this.element.style.boxShadow = NAV_BOX_SHADOW
-    this.element.style[EFFECT] = this.transition
+    css(this.element, {
+      [this.directionString]: response.dimension,
+      boxShadow: NAV_BOX_SHADOW[this.directionString],
+      [EFFECT]: this.transition
+    })
     this._body.style.overflow = HIDDEN
   }
 
   _moveHandler(response, rectangle) {
     let curPos = this.direction === Drawer.UP || this.direction === Drawer.DOWN ? rectangle.coordsY.y2 : rectangle.coordsX.x2
-    this.element.style[this.directionString] = response.dimension
-    this.element.style[EFFECT] = this.transition
+    css(this.element, {
+      [this.directionString]: response.dimension,
+      [EFFECT]: 'none',
+      [OVERFLOW]: HIDDEN
+    })
     if (this.direction === Drawer.RIGHT) {
       const WIN_SIZE = window.screen.availWidth
       curPos = WIN_SIZE - curPos
@@ -109,7 +121,8 @@ class NavDrawer {
   _threshold(state, stateObj) {
     const isOpen = state[1] === 'open'
     const options = {
-      stateObj
+      stateObj,
+      transition: `${this.directionString} ease ${this._calcSpeed(stateObj.TIMING) / KILO}s`
     }
     if (isOpen) {
       this._hide(options)
@@ -127,16 +140,20 @@ class NavDrawer {
     const displacement = this.direction === Drawer.UP || this.direction === Drawer.DOWN
       ? rect.displacementY : rect.displacementX
     const options = {
-      stateObj
+      stateObj,
+      transition: `${this.directionString} ease ${this._calcSpeed(stateObj.TIMING) / KILO}s`
     }
-    const LOGIC = this.direction === Drawer.LEFT
-      ? displacement > ZERO && displacement >= MPD && rect.greaterWidth
-      : displacement < ZERO && displacement <= MND && rect.greaterWidth
+    let LOGIC
+    if (this.direction === Drawer.LEFT && isClosed || this.direction === Drawer.RIGHT && !isClosed) {
+      LOGIC =  displacement > ZERO && displacement >= MPD && rect.greaterWidth
+    } else {
+      LOGIC = displacement < ZERO && displacement <= MND && rect.greaterWidth
+    }
 
     if (overallEventTime / KILO < MTTOB) {
       // DIRECTION: Drawer.UP | Drawer.LEFT
       if (LOGIC) {
-        this.__overrideBelowThresh(!isClosed, options)
+        this._overrideBelowThresh(!isClosed, options)
       } else {
         if (isClosed) {
           // close it back didn't hit thresh. and can't override
@@ -158,28 +175,86 @@ class NavDrawer {
   }
 
   _show(options) {
-    this._body.style.overflow = HIDDEN
-    this._backdrop.show(this.options.TRANSITION)
+    this._showPrep(options)
     this.element.style[this.directionString] = options.stateObj.dimension
   }
 
   _hide(options) {
-    this._body.style.overflow = SCROLL
-    this._backdrop.hide(this.options.TRANSITION)
+    this._hidePrep(options)
     this.element.style[this.directionString] = options.stateObj.dimension
-    this.element.style.boxShadow = 'none'
   }
 
-  __overrideBelowThresh(isOpen, options) {
+  _overrideBelowThresh(isOpen, options) {
     if (isOpen) {
-      this._body.style.overflow = SCROLL
-      this._backdrop.hide(this.options.TRANSITION)
+      this._hidePrep(options)
       this.element.style[this.directionString] = options.stateObj.oppositeDimension
-      this.element.style.boxShadow = 'none'
     } else {
-      this._body.style.overflow = HIDDEN
-      this._backdrop.show(this.options.TRANSITION)
+      this._showPrep(options)
       this.element.style[this.directionString] = options.stateObj.oppositeDimension
+    }
+  }
+
+  _hidePrep(options) {
+    this._body.style.overflow = SCROLL
+    this._backdrop.hide(this.options.TRANSITION)
+    css(this.element, {
+      [EFFECT]: options.transition,
+      [OVERFLOW]: SCROLL
+    })
+    if (!this.bound.lower) {
+      this.element.style.boxShadow = 'none'
+    }
+    this._setState('open')
+  }
+
+  _showPrep(options) {
+    window.location.hash = getAttribute(this.options.INIT_ELEM, HREF) || getData(this.options.INIT_ELEM, HASH_ATTR)
+    this._body.style.overflow = HIDDEN
+    this._backdrop.show(this.options.TRANSITION)
+    css(this.element, {
+      [EFFECT]: options.transition,
+      [OVERFLOW]: SCROLL
+    })
+    this._setState('open')
+  }
+
+  _calcSpeed(time) {
+    if (time >= MAX_TIME) {
+      return MAX_SPEED
+    }
+    const percent = 100
+    const percentage = time / MAX_TIME * percent
+    return percentage / percent * MAX_SPEED
+  }
+
+  _checkDirection() {
+    if (this.direction !== Drawer.LEFT && this.direction !== Drawer.RIGHT) {
+      throw RangeError('Direction out of range')
+    }
+  }
+
+  _setState(mode) {
+    switch (mode) {
+      case 'open':
+        STATE.navstate = {
+          alive: true,
+          activity: {
+            service: this,
+            action: mode
+          }
+        }
+        break
+      case 'close':
+        STATE.navstate = {
+          alive: false,
+          activity: {
+            service: this,
+            action: mode
+          }
+        }
+        break
+      default:
+        throw new Error('this should never happen')
     }
   }
 }
