2927192c09bad951f5cf3b065076e7ab72c99c4f
CalebPitan <36812893+CalebPitan@users.noreply.github.com>
2019-05-18 18:53:35 +0100

    improved API
    A navigation object is constructed by calling `new Nav()` the object is
    setup using the `setup` method, passing necessary parameters.
    The setup method returns a `NavMountWorker` object. The `mount`
    method of this object must be called to mount a Navigation activity
    and activate necessary services for the activity to run. The `unmount`
    method is used to unmount the activity and deactivate all associated
    services, even stubborn services are forced to deactivate.

--- src/nav/index.js
+++ src/nav/index.js
@@ -1,5 +1,6 @@
 import {
   $,
+  DIRECTIONS,
   css,
   getAttribute,
   getData,
@@ -19,26 +20,21 @@ const DEF_CLASSNAME = 'cardinal-navcard'
 const MEDIA_HASH = 'data-hash-max-width'
 const MEDIA_DRAW = 'data-draw-max-width'
 const _CLASS = 'class'
-const EVENTS = {
-  touchend: 'touchend',
-  touchmove: 'touchmove',
-  touchstart: 'touchstart'
-}
 
 class NavCard {
   constructor() {
-    // **Do not insert the below element into DOM**
     /**
-     * **Covert Backdrop**
-     * just incase `options.backdrop = false`
-     * prevent multiple `if` statements
+     * Covert Backdrop
+     * ----------------
+     * Just incase `options.backdrop = false`
+     * prevents multiple `if` statements
      * so we don't have to check whether
      * backdrop is enabled anytime we want to access it.
      *
      * Insert into DOM only:
      * when `options.backdrop = true`
      * `options.backdropClass` is undefined
-    */
+     */
     this.backdrop = document.createElement('div')
     this.backdrop.className = BACKDROP
     css(this.backdrop, {
@@ -48,43 +44,43 @@ class NavCard {
       display: 'none',
       position: 'fixed',
       top: 0,
-      left: 0,
-      zIndex: -1
+      left: 0
     })
 
     this.body = document.body
-    // init with null
     this.Drawer = null
     this.NavService = null
     this.HashState = null
-
-    NavCard.defaultConfig = {
-      type: 'nav',
-      transition: 500,
-      event: 'click',
-      direction: 'left',
-      backdrop: false,
-      backdropClass: null,
-      accessAttr: 'data-target',
-      maxStartArea: 25,
-      threshold: 1 / 2,
-      unit: 'px'
-    }
-
-    NavCard.API = {
-      DEFAULT: 1,
-      DRAWER: 3,
-      HASH: 7
-    }
   }
 
-  setup(_elem, options) {
-    let backdrop
-    let HASH_NAV_MAX_WIDTH
-    let NAV_DRAW_MAX_WIDTH
-    let destinationId
-    let destination
+  static defaultConfig = {
+    transition: 500,
+    direction: 'left',
+    backdrop: false,
+    backdropClass: null,
+    accessAttr: 'data-target',
+    maxStartArea: 25,
+    threshold: 1 / 2,
+    unit: 'px'
+  }
 
+  static SERVICES = {
+    Default: 2,
+    Drawer: 4,
+    Hash: 8
+  }
+
+  setup(el, options) {
+    if (!el) {
+      throw new TypeError('expected \'el\' to be selector string or HTMLElement')
+    }
+    let {
+      backdrop,
+      HASH_NAV_MAX_WIDTH,
+      NAV_DRAW_MAX_WIDTH,
+      destinationId,
+      destination
+    } = 1
     const opts = NavCard.defaultConfig
 
     if (options && typeof options === 'object') {
@@ -97,27 +93,12 @@ class NavCard {
       }
     }
 
-    if (opts.backdrop) {
-      const backdropclass = opts.backdropClass ? opts.backdropClass : false
-      // if `opts.backdrop` and no `backdropclass` given
-      // append our custom backdrop
-      if (!backdropclass) {
-        this.body.append(this.backdrop)
-      }
-
-      if (typeof backdropclass === 'string') {
-        // check if backdropclass is normal string or css class selector
-        backdrop =  /^\./.test(backdropclass) ? backdropclass : `.${backdropclass}`
-        this.backdrop = $(backdrop)
-      }
-    }
-
     const dataAccess = opts.accessAttr
 
     /**
      * Initialization element e.g <button ...</button>
      */
-    const element = $(_elem)
+    const element = el instanceof HTMLElement ? el : $(el)
     destinationId = getData(element, dataAccess)
 
     // check if the data-* attribute value is a valid css selector
@@ -128,7 +109,7 @@ class NavCard {
 
     destination = $(destinationId)
 
-    // we want a class too so in case we've got an id selector
+    // we want a class too, so in case we've got an id selector
     // find the classname or assign a unique class
     const defaultClass = `${DEF_CLASSNAME}-${unique(2 << 7)}`
     // eslint-disable-next-line no-nested-ternary
@@ -139,11 +120,21 @@ class NavCard {
     classname = '.' + (classlist.length >= 2 ? classlist[0] + '.' + classlist[1] : classname)
     destination = isClass ? destination : $(classname)
 
-    // These attributes are used for the RWD(Responsive Web Design)
-    // features. The navigation drawer module is best suited for mobile
-    // touch devices. The program shouldn't listen for certain events on
-    // desktop devices as side-nav may be hidden. The attribute here are
-    // the screen sizes after which the nav is hidden.
+    if (opts.backdrop) {
+      const backdropclass = opts.backdropClass || false
+      // if `opts.backdrop` and no `backdropclass` given
+      // append our custom backdrop
+      if (!backdropclass) {
+        destination.insertAdjacentElement('beforeBegin', this.backdrop)
+      }
+
+      if (typeof backdropclass === 'string') {
+        // check if backdropclass is normal string or css class selector
+        backdrop =  /^\./.test(backdropclass) ? backdropclass : `.${backdropclass}`
+        this.backdrop = $(backdrop)
+      }
+    }
+
     HASH_NAV_MAX_WIDTH = getData(destination, MEDIA_HASH)
     NAV_DRAW_MAX_WIDTH = getData(destination, MEDIA_DRAW)
     HASH_NAV_MAX_WIDTH = /px$/.test(HASH_NAV_MAX_WIDTH) ? HASH_NAV_MAX_WIDTH : `${HASH_NAV_MAX_WIDTH}px`
@@ -155,13 +146,12 @@ class NavCard {
       BACKDROP: new Backdrop(this.backdrop),
       BODY: this.body,
       TRANSITION: opts.transition,
-      DIRECTION: ['top', 'left', 'bottom', 'right'][opts.direction],
+      DIRECTION: DIRECTIONS[opts.direction],
       unit: options.unit
     }
     const drawerOptions = {
       ...defaultOptions,
       MAX_WIDTH: NAV_DRAW_MAX_WIDTH,
-      ...EVENTS,
       DIRECTION: opts.direction,
       maxStartArea: opts.maxStartArea,
       threshold: opts.threshold
@@ -171,9 +161,17 @@ class NavCard {
       MAX_WIDTH: HASH_NAV_MAX_WIDTH
     }
 
-    this._drawerAPI(drawerOptions).activate()
-    this._hashAPI(hashOptions).activate()
-    this._defaultAPI(defaultOptions).activate()
+    return new NavMountWorker(Object.assign({}, {
+      defaultOptions
+    }, {
+      drawerOptions
+    }, {
+      hashOptions
+    }))
+  }
+
+  toString() {
+    return '[object NavCard]'
   }
 
   _drawerAPI(options) {
@@ -185,7 +183,7 @@ class NavCard {
   }
 
   _hashAPI(options) {
-    this.HashState = new HashState(options)
+    this.HashState = new HashState(this.NavService, options)
     return {
       activate: () => this.HashState.activate(),
       deactivate: () => this.HashState.deactivate()
@@ -200,29 +198,46 @@ class NavCard {
     }
   }
 
-  terminate(state) {
+  terminate(service) {
     // this._*API(null).deactivate()
-    switch (state) {
-      case NavCard.API.DEFAULT:
+    switch (service) {
+      case NavCard.SERVICES.Default:
         if (this.NavService instanceof NavService) {
           this.NavService.deactivate()
         }
         break
-      case NavCard.API.DRAWER:
+      case NavCard.SERVICES.Drawer:
         if (this.Drawer instanceof NavDrawer) {
           this.Drawer.deactivate()
         }
         break
-      case NavCard.API.HASH:
+      case NavCard.SERVICES.Hash:
         if (this.HashState instanceof HashState) {
           this.HashState.deactivate()
         }
         break
       default:
-        this._drawerAPI(null).deactivate()
-        this._hashAPI(null).deactivate()
-        this._defaultAPI(null).deactivate()
+        throw new Error('a service id is required')
     }
+  }
+}
+
+class NavMountWorker extends NavCard {
+  constructor(options) {
+    super()
+    this.options = options
+  }
+
+  mount() {
+    this._defaultAPI(this.options.defaultOptions).activate()
+    this._drawerAPI(this.options.drawerOptions).activate()
+    this._hashAPI(this.options.hashOptions).activate()
+  }
+
+  unmount() {
+    this.NavService.forceDeactivate()
+    this.Drawer.deactivate()
+    this.HashState.deactivate()
   }
 }
 
