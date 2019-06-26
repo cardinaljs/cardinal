03750b9e510013ef9516d01a95ce3d2ea3d5c83b
CalebPitan <36812893+CalebPitan@users.noreply.github.com>
2019-06-26 17:55:26 +0100

    provides a DrawerManagerStore
    The DrawerManagerStore manages multiple drawers actvities that uses the
    drawer service
    
    It helps prevent more than one drawer service from running at a time
    causing disruption

--- src/drawer/index.js
+++ src/drawer/index.js
@@ -1,5 +1,19 @@
 import SnappedDrawer from './snapped-drawer'
 
+class DrawerManagementStore {
+  constructor() {
+    this.activities = []
+  }
+
+  pushActivity(activity) {
+    this.activities.push(activity)
+  }
+
+  getRunningActivity() {
+    return this.activities.find((activity) => activity.isRunning())
+  }
+}
+
 export default class Drawer {
   static SnappedDrawer = SnappedDrawer
   // enum
@@ -7,4 +21,5 @@ export default class Drawer {
   static LEFT = SnappedDrawer.LEFT
   static DOWN = SnappedDrawer.DOWN
   static RIGHT = SnappedDrawer.RIGHT
+  static DrawerManagementStore = new DrawerManagementStore()
 }
